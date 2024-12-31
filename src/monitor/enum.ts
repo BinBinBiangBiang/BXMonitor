bee4d52fd998b287e55bfa2f0806c28df9fd4f46
BinBinBiangBiang <3122583749.com>
2024-12-31 16:27:26 +0800

    feat(枚举): 枚举值修改

--- src/monitor/enum.ts
+++ src/monitor/enum.ts
@@ -1,8 +1,9 @@
 export enum MonitorType {
-  performance = 'performance', // 性能监控
-  click = 'click', // 点击埋点
-  error = 'error', // 错误监控
-  show = 'show', // 曝光埋点
-  custom = 'custom', // 自定义埋点
-  promiseError = 'promise_error' // promise 错误监控
+  PERFORMANCE = 'performance', // 性能监控
+  CLICK = 'click', // 点击埋点
+  ERROR = 'error', // 错误监控
+  PAGE_VIEW = 'pageView', // 页面曝光埋点
+  PAGE_VIEW_OUT = 'pageViewOut', // 页面离开埋点
+  MODULE_EXPOSE = 'moduleExpose', // 模块曝光埋点
+  PROMISE_ERROR = 'promise_error' // promise 错误监控
 }
